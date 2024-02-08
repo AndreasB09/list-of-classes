244743a50e96953425bf502062d38427ccc84bee
andreasb09 <andreasb09.kodehode@gmail.com>
2024-02-08 14:40:59 +0100

    added img to header

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,11 @@ import styles from "./Footer.module.css";
 
 export function Footer() {
   return (
-    <footer className={styles.footer}>
-      <h1>This is a footer</h1>
-    </footer>
+    <div className={styles.footerContainer}>
+      <footer className={styles.footer}>
+        <h1 className={styles.title}>This is a footer</h1>
+      </footer>
+    </div>
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
+import githubLogo from "./assets/images/github_logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
       <h1>This is a header</h1>
+      <img className={styles.logo} src={githubLogo} alt="GitHub Logo"/>
     </header>
   );
 }
